d208667ff55d51e983c01314d355b1a9cc45e950
Julian Gutierrez Ferrara <juliangutierrezferrara@gmail.com>
2018-06-10 15:47:47 -0400

    Empty collection view with search bar

---
+++ GiphSearcher/GiphyCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  GiphyCollectionViewCell.swift
+//  GiphSearcher
+//
+//  Created by Julian Gutierrez on 6/10/18.
+//  Copyright © 2018 Jugutier. All rights reserved.
+//
+
+import UIKit
+
+class GiphyCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var imageView: UIImageView!
+}
---
+++ GiphSearcher/GiphyCollectionViewController.swift
@@ -0,0 +1,63 @@
+//
+//  GiphyCollectionViewController.swift
+//  GiphSearcher
+//
+//  Created by Julian Gutierrez on 6/10/18.
+//  Copyright © 2018 Jugutier. All rights reserved.
+//
+
+import UIKit
+
+private let reuseIdentifier = "GiphyCollectionCell"
+private let sectionInsets = UIEdgeInsets(top: 50.0, left: 20.0, bottom: 50.0, right: 20.0)
+private let itemsPerRow: CGFloat = 3
+
+class GiphyCollectionViewController: UICollectionViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.collectionView!.register(UICollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
+    }
+
+    // MARK: UICollectionViewDataSource
+
+    override func numberOfSections(in collectionView: UICollectionView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+
+
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of items
+        return 0
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath)
+        // Configure the cell
+        return cell
+    }
+}
+
+
+extension GiphyCollectionViewController : UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let paddingSpace = sectionInsets.left * (itemsPerRow + 1)
+        let availableWidth = view.frame.width - paddingSpace
+        let widthPerItem = availableWidth / itemsPerRow
+        
+        return CGSize(width: widthPerItem, height: widthPerItem)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        insetForSectionAt section: Int) -> UIEdgeInsets {
+        return sectionInsets
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return sectionInsets.left
+    }
+}
