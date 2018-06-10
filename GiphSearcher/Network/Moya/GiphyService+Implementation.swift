d19db3c8b27af371b28462e955530f49c2ae0e45
Julian Gutierrez Ferrara <juliangutierrezferrara@gmail.com>
2018-06-10 13:21:38 -0400

    Add moya service stub

---
+++ GiphSearcher/Network/Moya/GiphyService+Implementation.swift
@@ -0,0 +1,38 @@
+//
+//  GiphyService+Implementation.swift
+//  GiphSearcher
+//
+//  Created by Julian Gutierrez on 6/10/18.
+//  Copyright © 2018 Jugutier. All rights reserved.
+//
+
+import UIKit
+import Moya
+
+// MARK: - TargetType Protocol Implementation
+extension GiphyService: TargetType {
+    var baseURL: URL {
+        //TODO
+    }
+    
+    var path: String {
+        //TODO
+    }
+    
+    var method: Moya.Method {
+        //TODO
+    }
+    
+    var sampleData: Data {
+        //TODO
+    }
+    
+    var task: Task {
+        //TODO
+    }
+    
+    var headers: [String : String]? {
+        //TODO
+    }
+    
+}
