a8aa2e68b0c32fc03f18b8cac82d3813750a8f75
Julian Gutierrez Ferrara <juliangutierrezferrara@gmail.com>
2018-06-10 17:50:18 -0400

    Add collection section view

--- GiphSearcher/GiphyCollectionViewController.swift
+++ GiphSearcher/GiphyCollectionViewController.swift
@@ -9,6 +9,7 @@
 import UIKit
 
 private let reuseIdentifier = "GiphyCollectionCell"
+private let sectionReuseIdentifier = "GiphySection"
 private let sectionInsets = UIEdgeInsets(top: 50.0, left: 20.0, bottom: 50.0, right: 20.0)
 private let itemsPerRow: CGFloat = 3
 
